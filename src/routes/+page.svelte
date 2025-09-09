dcb8225e53169a83b6608d1d5a5a018d01083ac0
Priscilla (Laila Sandra) <a.g.marosi@bluewin.ch>
2025-09-09 22:10:09 +0200

    Made tabs work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
 import {invoke} from "@tauri-apps/api/core";
-// import "../../static/bootstrap.css";
-// import "$static/bootstrap.css";
+import wsl_logo from "../Logo_WSL.svg";
+
+const ONE_TIME_DATA_TAB = 1;
+const INPUT_DATA_TAB = 2;
+const DATA_TAB = 3;
+
+const MAX_DIAMETER = 300; // [cm]
+const MAX_DISTANCE = 25; // [m]
+const MAX_LENGTH = 50; // [m]
+const MIN_DIAMETER_1 = 7; // [cm]
+const MIN_LENGTH_1 = 2; // [m]
+const MIN_DIAMETER_2 = 36; // [cm]
+const MIN_LENGTH_2 = 0.5; // [m]
 
 const tree_species = [
   [-1, "unknown"],
@@ -12,8 +23,7 @@ const decay_states = [1, 2, 3, 4, 5];
 
 // https://svelte.dev/playground/052c877eb34c45ee8f773a8bf8475347?version=5.38.6#H4sIAAAAAAAAA5VVUW_TMBD-K0cAJRVN2iF48ZpKlZgmBGLS2Atq--AkbmuWOMF2ulRZ_ztnO9lSmDZ4qOSc7777vvNnt_UELZhHvM-iqjXsac4zqnkpvLG34TlTHlm2nj5UJscEMN5VLKoqUnuWaxNLqGJPxdNSaCY0wngzlUpe6flKrHTONOhS0xxiOJtOz_sYNywUBpetCa00EqoZgenYfaZlXkoC_uX14odvQscxPJ_5aXH95fvXxc3Fv5fcXCy-usy1JfaGgKqu9I5Jw8zRDjuqUUGroIF4Dk1k4eD-HqajSLKsTlkQ0DEkI7NN4R2uLJ75bWqRmjH3E2cBFxlrRj01M4yCNu5UXmw6ivBoNJNB0IyB234cXsUxONDn2Wi-gcABL23-ulMyf2DwQAuTn0iMHxI7xKOdntM6mzyeu5gpfciZtUBkkUJjEMoFk12HjKsqpwcCm5w1HRyOaCtCrlmhCKRoJya7nZ-1QvqHsLOZqwqVprKncsczvSPGZG_PB6ywv_pVU8naYdpZ9FGyoqvcMb7d6dNYQeWWi1DaHYg-PO4kpcyYDCXNeI0so_em6rShFdwO0wmIUrAOoax1joM4iXWoSal1WaCKqgFVomFA8XzP5BP4YWmN2reh6e1WlrXIws7dWlKhKhQu9Em1LV6aix6LukiYXBMS3rHkluuQCzyfUFWoPKmRiRjDSyUo5rQEWnCU-gxaVYwimbRXDMMR4610gaN1UO8a0b5mNN317wRV0FoLjsHKw6vNraJZxveQ5lSp2P_DZ_7c9RmmOCv4YPvEK-_vsbUOf-XBpK-3uGDlrzw3ANwuuMDPKa6G7X1I8MYQyzV2lI9QCmI34zawl_LxKRgduzazCbLEVTsxss0sXhL2f7LM63h1vfh2efGgbKjLd7L8k44w9NmpsP6ZPAJeY5rkLDOYvQj8O9Cs0R7REuWv8Yvy_A7LPbKhuWLH3zmV9pKNBgAA
 
-let name = $state("");
-let greetMsg = $state("");
+let current_tab = $state(ONE_TIME_DATA_TAB);
 
 let area_group = $state(0);
 let subarea = $state(0);
@@ -22,7 +32,6 @@ let start_date = $state("");
 let piece_id = $state(0);
 let piece_id_error = $state("Kein Wert vorhanden");
 function piece_id_validator() {
-  console.log(`piece_id: ${piece_id}`);
   if (piece_id < 1) {
     piece_id_error = "Must be > 1";
   } else {
@@ -32,14 +41,24 @@ function piece_id_validator() {
 
 let part_id = $state(0);
 let part_id_error = $state("");
-function part_id_validator() {}
+function part_id_validator() {
+  // Todo: implement :^)
+}
 
 let species = $state(-1);
 let species_error = $state("Keine valide Spezies");
 
 let d1_max = $state(0.0);
 let d1_max_error = $state("Kein Wert vorhanden");
-function d1_max_validator() {}
+function d1_max_validator() {
+  if (d1_max > MAX_DIAMETER) {
+    d1_max_error = `Wert muss kleiner als ${MAX_DIAMETER} sein`;
+  } else if (d1_max < MIN_DIAMETER_1) {
+    d1_max_error = `Wert muss grösser sein als ${MIN_DIAMETER_1} sein`;
+  } else {
+    d1_max_error = "";
+  }
+}
 
 let d2_max = $state(0.0);
 let d2_max_error = $state("Kein Wert vorhanden");
@@ -92,6 +111,11 @@ let former_tree_nr = $state(0);
 let former_tree_nr_error = $state("");
 function former_tree_nr_validator() {}
 
+let comments = $state("");
+
+let name = $state("");
+let greetMsg = $state("");
+
 async function greet(event: Event) {
   event.preventDefault();
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -100,21 +124,43 @@ async function greet(event: Event) {
 </script>
 
 <main class="container-fluid">
-  <h1 class="h1">Liegende Totholzapp</h1>
+  <!-- Todo: this mess -->
+  <header class="row row-cols-auto align-items-start w-100">
+    <h1 class="h1 col">Liegende Totholzapp</h1>
+    <p class="col align-self-start justify-content-end">Made by and for:</p>
+    <img
+      class="img-fluid mw-100 col justify-content-end"
+      src={wsl_logo}
+      alt="WSL Logo"
+      style="height: 3rem;"
+    />
+  </header>
 
-  <ul class="nav nav-pills">
+  <ul class="nav nav-pills mb-4">
     <li class="nav-item">
-      <a class="nav-link active" href="#">Einmalige Daten</a>
+      <button
+        class="nav-link"
+        class:active={current_tab == ONE_TIME_DATA_TAB}
+        onclick={() => (current_tab = ONE_TIME_DATA_TAB)}>Einmalige Daten</button
+      >
     </li>
     <li class="nav-item">
-      <a href="#" class="nav-link">Neue Daten</a>
+      <button
+        class="nav-link"
+        class:active={current_tab == INPUT_DATA_TAB}
+        onclick={() => (current_tab = INPUT_DATA_TAB)}>Neue Daten</button
+      >
     </li>
     <li class="nav-item">
-      <a href="#" class="nav-link">Aufgenommene Daten</a>
+      <button
+        class="nav-link"
+        class:active={current_tab == DATA_TAB}
+        onclick={() => (current_tab = DATA_TAB)}>Aufgenommene Daten</button
+      >
     </li>
   </ul>
 
-  <div class="one-time-data container text-start">
+  <div class="one-time-data container text-start" class:d-none={current_tab != ONE_TIME_DATA_TAB}>
     <label class="form-label" for="area_group">Flächengruppe:</label>
     <input
       class="form-control mb-4"
@@ -153,11 +199,11 @@ async function greet(event: Event) {
 
   <!-- Todo: team -->
 
-  <div class="input container text-start">
+  <div class="input container text-start" class:d-none={current_tab != INPUT_DATA_TAB}>
     <label class="form-label" for="piece_id">Stück ID:</label>
     <input
       class="form-control"
-      class:mb-4={piece_id_error == ""}
+      class:mb-4={!piece_id_error}
       bind:value={piece_id}
       onchange={piece_id_validator}
       onkeypress={piece_id_validator}
@@ -169,7 +215,7 @@ async function greet(event: Event) {
       step="1"
       required
     />
-    {#if piece_id_error != ""}
+    {#if piece_id_error}
       <div class="alert alert-warning form-text mb-4">{piece_id_error}</div>
     {/if}
 
@@ -177,7 +223,7 @@ async function greet(event: Event) {
     <input
       type="number"
       class="form-control"
-      class:mb-4={part_id_error == ""}
+      class:mb-4={!part_id_error}
       bind:value={part_id}
       onchange={part_id_validator}
       onkeypress={part_id_validator}
@@ -186,7 +232,7 @@ async function greet(event: Event) {
       max="99"
       step="1"
     />
-    {#if part_id_error != ""}
+    {#if part_id_error}
       <div class="alert alert-warning form-text mb-4">{part_id_error}</div>
     {/if}
 
@@ -225,10 +271,11 @@ async function greet(event: Event) {
       </optgroup>
     </select> -->
 
+    <!-- TODO: add units to unit measurements -->
     <label for="d1_max" class="form-label">Durchmesser 1 max.:</label>
     <input
       class="form-control"
-      class:mb-4={piece_id_error == ""}
+      class:mb-4={!piece_id_error}
       bind:value={d1_max}
       onchange={d1_max_validator}
       onkeypress={d1_max_validator}
@@ -239,14 +286,14 @@ async function greet(event: Event) {
       max="99"
       step="0.1"
     />
-    {#if d1_max_error != ""}
+    {#if d1_max_error}
       <div class="alert alert-warning form-text mb-4">{d1_max_error}</div>
     {/if}
 
     <label for="d2_max" class="form-label">Durchmesser 2 max.:</label>
     <input
       class="form-control"
-      class:mb-4={d2_max_error == ""}
+      class:mb-4={!d2_max_error}
       bind:value={d2_max}
       onchange={d2_max_validator}
       onkeypress={d2_max_validator}
@@ -257,14 +304,14 @@ async function greet(event: Event) {
       max="99"
       step="0.1"
     />
-    {#if d2_max_error != ""}
+    {#if d2_max_error}
       <div class="alert alert-warning form-text mb-4">{d2_max_error}</div>
     {/if}
 
     <label for="azimax" class="form-label">Azimuth max.:</label>
     <input
       class="form-control"
-      class:mb-4={azimax_error == ""}
+      class:mb-4={!azimax_error}
       bind:value={azimax}
       onchange={azimax_validator}
       onkeypress={azimax_validator}
@@ -275,17 +322,17 @@ async function greet(event: Event) {
       max="99"
       step="0.1"
     />
-    {#if azimax_error != ""}
+    {#if azimax_error}
       <div class="alert alert-warning form-text mb-4">{azimax_error}</div>
     {/if}
 
     <label for="distmax" class="form-label">Distanz max.:</label>
     <input
       class="form-control"
-      class:mb-4={distmax_error == ""}
+      class:mb-4={!distmax_error}
       bind:value={distmax}
       onchange={distmax_validator}
-      onkeydown={distmax_validator}
+      onkeypress={distmax_validator}
       type="number"
       name="distmax"
       id="distmax"
@@ -293,14 +340,14 @@ async function greet(event: Event) {
       max="99"
       step="0.1"
     />
-    {#if distmax_error != ""}
+    {#if distmax_error}
       <div class="alert alert-warning form-text mb-4">{distmax_error}</div>
     {/if}
 
     <label for="d1_min" class="form-label">Durchmesser 1 min.:</label>
     <input
       class="form-control"
-      class:mb-4={d1_min_error == ""}
+      class:mb-4={!d1_min_error}
       bind:value={d1_min}
       onchange={d1_min_validator}
       onkeypress={d1_min_validator}
@@ -311,14 +358,14 @@ async function greet(event: Event) {
       max="99"
       step="0.1"
     />
-    {#if d1_min_error != ""}
+    {#if d1_min_error}
       <div class="alert alert-warning form-text mb-4">{d1_min_error}</div>
     {/if}
 
     <label for="d2_min" class="form-label">Durchmesser 2 min.:</label>
     <input
       class="form-control"
-      class:mb-4={d2_min_error == ""}
+      class:mb-4={!d2_min_error}
       bind:value={d2_min}
       onchange={d2_min_validator}
       onkeypress={d2_min_validator}
@@ -329,14 +376,14 @@ async function greet(event: Event) {
       max="99"
       step="0.1"
     />
-    {#if d2_min_error != ""}
+    {#if d2_min_error}
       <div class="alert alert-warning form-text mb-4">{d2_min_error}</div>
     {/if}
 
     <label for="azimin" class="form-label">Azimuth min.:</label>
     <input
       class="form-control"
-      class:mb-4={azimin_error == ""}
+      class:mb-4={!azimin_error}
       bind:value={azimax}
       onchange={azimin_validator}
       onkeypress={azimin_validator}
@@ -347,14 +394,14 @@ async function greet(event: Event) {
       max="99"
       step="0.1"
     />
-    {#if azimin_error != ""}
+    {#if azimin_error}
       <div class="alert alert-warning form-text mb-4">{azimin_error}</div>
     {/if}
 
     <label for="distmin" class="form-label">Distanz min.:</label>
     <input
       class="form-control"
-      class:mb-4={distmin_error == ""}
+      class:mb-4={!distmin_error}
       bind:value={distmin}
       onchange={distmin_validator}
       onkeypress={distmin_validator}
@@ -365,14 +412,14 @@ async function greet(event: Event) {
       max="99"
       step="0.1"
     />
-    {#if distmax_error != ""}
+    {#if distmax_error}
       <div class="alert alert-warning form-text mb-4">{distmin_error}</div>
     {/if}
 
     <label for="length" class="form-label">Länge</label>
     <input
       class="form-control"
-      class:mb-4={length_error == ""}
+      class:mb-4={!length_error}
       bind:value={length}
       onchange={length_validator}
       onkeypress={length_validator}
@@ -383,10 +430,10 @@ async function greet(event: Event) {
       max="99"
       step="0.1"
     />
-    {#if length_error != ""}
+    {#if length_error}
       <div class="alert alert-warning form-text">{length_error}</div>
     {/if}
-    {#if min_vals_error != ""}
+    {#if min_vals_error}
       <div class="alert alert-warning from-text mb-4">{min_vals_error}</div>
     {/if}
 
@@ -395,7 +442,7 @@ async function greet(event: Event) {
     <label class="form-label" for="ref_tree_nr">Referenz Baum ID:</label>
     <input
       class="form-control"
-      class:mb-4={ref_tree_nr_error == ""}
+      class:mb-4={!ref_tree_nr_error}
       bind:value={ref_tree_nr}
       onchange={ref_tree_nr_validator}
       onkeypress={ref_tree_nr_validator}
@@ -413,7 +460,7 @@ async function greet(event: Event) {
     <label class="form-label" for="former_tree_nr">Ehem. Baum ID:</label>
     <input
       class="form-control"
-      class:mb-4={former_tree_nr_error == ""}
+      class:mb-4={!former_tree_nr_error}
       bind:value={former_tree_nr}
       onchange={former_tree_nr_validator}
       onkeypress={former_tree_nr_validator}
@@ -424,12 +471,19 @@ async function greet(event: Event) {
       max="999999"
       step="1"
     />
-    {#if former_tree_nr_error != ""}
+    {#if former_tree_nr_error}
       <div class="alert alert-warning form-text mb-4">{former_tree_nr_error}</div>
     {/if}
+
+    <label for="comments" class="form-label">Bemerkung(en)</label>
+    <textarea name="comments" id="comments" class="form-control mb-4"></textarea>
+
+    <button class="btn btn-primary mb-4 disabled">Daten aufnehmen</button>
   </div>
 
-  <div class="data container text-start"></div>
+  <div class="data container text-start" class:d-none={current_tab != DATA_TAB}>
+    <h1 class="h1">Aufgenommene Daten</h1>
+  </div>
 
   <!-- Todo: use CSS:grid for this and split into 3 columns: label, input, error message -->
   <!-- <div class="vertical inputs">
